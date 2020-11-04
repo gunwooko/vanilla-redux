ff7f37a2eb5dc7468b08b81da80731c4bb0623b9
gunwooko <kkw722@gmail.com>
2020-11-04 18:27:35 +0900

    Installed Redux, Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
+import { createStore } from "redux"; // create a place where data will be stored
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+// // reducer is a function that modify our data and whatever reducer function returns, it becomes our data
+// // And Reducer function will be a place where only can change our data => state
+// const reducer = () => { return "something"};
+// // reducer function is necessry to create store
+// const store = createStore(reducer);
 
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0) => {
+  // Here is going to modify the state (in this case => count)
+  return count;
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
