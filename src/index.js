c14fef6c752a9165e76a5abc721b504431fc4b17
gunwooko <kkw722@gmail.com>
2020-11-05 17:08:51 +0900

    Refactored

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD"; // I changed types strings to variables because JS will tell me if something is wrong with variables.
+const MINUS = "MINUS";
+
 // // reducer is a function that modify our data and whatever reducer function returns, it becomes our data
 // // And Reducer function will be a place where only can change our data => state
 // const reducer = () => { return "something"};
@@ -12,16 +15,28 @@ const number = document.querySelector("span");
 
 const countModifier = (count = 0, action) => {
   // action is a second parameter, Action is the way that we comunicate with the countModifier (reducer)
-  // the action has to be an object
+  // the action has to be an object And action MUST have a type
   // dispatch method sends an action to reducer function
   // Here is going to modify the state (in this case => count)
   // console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+
+  // // option 1
+  // if (action.type === "ADD") {
+  //   return count + 1;
+  // } else if (action.type === "MINUS") {
+  //   return count - 1;
+  // } else {
+  //   return count;
+  // }
+
+  // option 2 <improvement>
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -36,10 +51,10 @@ const onChange = () => {
 countStore.subscribe(onChange); // subscribe method allows us to listen for changes in our store
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
